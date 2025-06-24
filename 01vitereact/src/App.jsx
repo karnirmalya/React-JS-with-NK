85ae38951d3dff9b0c5f51bc36d8da4557fd546e
nirmakar24 <karnirmalya2003@gmail.com>
2025-06-24 10:21:51 +0530

    custom react created and understood

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,17 +1,22 @@
+import Chai from "./one.jsx";
 
-
-import Chai from  './one.jsx'
-
-
-function App() {
-  return (
-    <>
-    <Chai />
-    <h1>Hello Good Evening </h1>
-    <p>Hello i am learing React now </p>
-    </>
-   
-  )
+function myApp() {
+  return <div>Custom App</div>;
 }
 
-export default App
+function App() {
+  const username = "Nirmalya Kar";
+  return (
+    <>
+      <Chai />
+      {/* {} :-  Evaluation expression(basically taking the value of the variable after calculating)
+       (Expression value will be calculated inside or outside the function)
+        (For clarification look at main .jsx and  evaluation cant be done since we can't wtite an expresion as the object entity )*/}
+      <h1>Hello Good Evening {username} </h1>
+      <p>Hello i am learing React now </p>
+    </>
+  );
+}
+
+export default App;
+// export default myApp
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+function MyApp() {
+  return (
+    <div>
+      <h1>My Custom App!</h1>
+    </div>
+  );
+}
 
-    <App />
+//CUSTOM REACT(OBJECT)
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank"
+//   },
+//   children: 'Click me to visit Google'
+// };
 
-)
+const AnotherElement = (
+  <a href="https://google.com" target="_blank">
+    VISIT GOOGLE
+  </a>
+);
+
+// const reactElement = React.createElement(
+//   'a',
+//   {
+//     href: "https://google.com",
+//     title: "My Paragraph",
+//     target: "_blank",
+//   },
+//   "Click me to Visit Google"
+// );
+
+const anotherUser = ' Code With Nirmalya'
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google", 
+  anotherUser
+);
+
+createRoot(document.getElementById("root")).render(
+     reactElement
+  // AnotherElement
+//   <App />
+);
