36caa047909d4e44f8bd392ffdc02b374080bf1e
nirmakar24 <karnirmalya2003@gmail.com>
2025-07-11 09:29:13 +0530

    React-router crash course

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+
+
+
+function App() {
+
+  return (
+   <>
+    
+   </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/components/About/Nirmalya.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Nirmalya() {
+  return (
+    <div>
+      <h1>Hello my self NIRMALYA KAR</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias non ipsam quaerat, fugit eum libero magni cum unde iusto incidunt! Non esse iste totam asperiores omnis suscipit, nobis voluptatum, dignissimos praesentium quos, perspiciatis sint sit perferendis illum doloremque deleniti assumenda sapiente aut. Ut ratione dolorem cupiditate id quisquam nulla sed! Fuga officiis placeat sunt aut dolorum! Ipsa velit mollitia quod.</p>
+    </div>
+  )
+}
+
+export default Nirmalya
