2706b68739bbeac1c0f113e2deea19921aaf0969
nirmakar24 <karnirmalya2003@gmail.com>
2025-09-08 09:20:39 +0530

    fix: update import paths to match directory casing for consistency

--- 10todoContextLocal/src/components/TodoItem.jsx
+++ 10todoContextLocal/src/components/TodoItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useTodo } from '../contexts/TodoContext';
+import { useTodo } from '../Contexts';
 
 function TodoItem({ todo }) {
   const [isTodoEditable, setIsTodoEditable] = useState(false)
